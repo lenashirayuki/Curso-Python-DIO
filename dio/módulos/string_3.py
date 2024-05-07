569e24fd045150753f0f359ee175eece6d09c5dc
lenashirayuki <helenayasmindcarvalho@gmail.com>
2024-05-07 10:37:24 -0300

    Commit 6

---
+++ dio/módulos/string_3.py
@@ -0,0 +1,8 @@
+nome = "Helena Yasmin de Carvalho"
+
+print(nome[0])
+print(nome[:7])
+print(nome[7:])
+print(nome[7:14])
+print(nome[7:14:2])
+print(nome[::-1])
---
+++ dio/módulos/string_4.py
@@ -0,0 +1,10 @@
+nome = "Helena"
+
+mensagem = f'''
+  Olá, meu nome é {nome},
+E eu estou aprendendo Python. 
+        Gostaria de me conhecer?
+'''
+
+print(mensagem)
+
